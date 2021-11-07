2a63b745579e4b4cfc39d1bd70adf7e3d24acc06
Christopher Caldwell <62559469+christopher-caldwell@users.noreply.github.com>
2021-11-07 10:24:04 -0700

    feat: Adding results for two sum

--- src/easy/two-sum/index.test.ts
+++ src/easy/two-sum/index.test.ts
@@ -3,6 +3,19 @@ import { twoSum } from './index'
 describe('Two Sums', () => {
   test('nums = [2,7,11,15], target = 9', () => {
     const result = twoSum([2, 7, 11, 15], 9)
+    console.log('result', result)
     expect(result).toStrictEqual([0, 1])
+  })
+  test('nums = [3,2,4], target = 6', () => {
+    const result = twoSum([3, 2, 4], 6)
+    expect(result).toStrictEqual([1, 2])
+  })
+  test('nums = [3,3], target = 6', () => {
+    const result = twoSum([3, 3], 6)
+    expect(result).toStrictEqual([0, 1])
+  })
+  test('nums = [2,5,5,11], target = 10', () => {
+    const result = twoSum([2, 5, 5, 11], 10)
+    expect(result).toStrictEqual([1, 2])
   })
 })
--- src/easy/two-sum/index.ts
+++ src/easy/two-sum/index.ts
@@ -1,16 +1,44 @@
+/**
+ * My solution
+ * @speed 3114ms
+ */
 export function twoSum(nums: number[], target: number): number[] {
+  const resultMap: Record<number, number[]> = {}
   let firstIndex = 0
-  let secondIndex = 1
-  let finished = false
-  while (firstIndex < nums.length || !finished) {
-    for (let iterator = secondIndex; iterator <= nums.length; iterator++) {
-      if (nums[firstIndex] + nums[iterator] === target) {
-        secondIndex = iterator
-        finished = true
+  let isFinished = false
+  while (firstIndex < nums.length && !isFinished) {
+    for (let iterator = 0; iterator < nums.length; iterator++) {
+      if (iterator === firstIndex) continue
+      const sum = nums[firstIndex] + nums[iterator]
+      resultMap[sum] = [firstIndex, iterator]
+      if (sum === target) {
+        isFinished = true
         break
       }
     }
     firstIndex++
   }
-  return [firstIndex, secondIndex]
+  return resultMap[target]
+}
+
+/**
+ * Found solution. Comments assume input is `fastTwoSum([2,5,5,11], 10)`
+ * @speed 80ms
+ */
+export function fastTwoSum(nums: number[], target: number): number[] {
+  const dict: Map<number, number> = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    // if the map has the value at the current index, then it means the pair has been found
+    if (dict.has(nums[i])) {
+      // get the value ( index ) at 5, and return it, and the current index: [1, 2]
+      return [dict.get(nums[i])!, i]
+    }
+
+    // Setting the insverse of the target. 10 - 5 = 5, so 5 is set as the key, with the current index as the value.
+    // 5 is set because if the loop finds another 5, it means the result is found ( 10 )
+    dict.set(target - nums[i], i)
+  }
+
+  return []
 }
