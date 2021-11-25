ae52f778c4ac4dffa6bcb5fd7a43077c7fe313c0
christopher-caldwell <well.done.514@gmail.com>
2021-11-24 23:34:59 -0700

    feat: finishing length of last word, slowly

--- src/easy/palindrome-number/index.ts
+++ src/easy/palindrome-number/index.ts
@@ -2,12 +2,8 @@
  * @speed 208 ms
  * @memory 48 MB
  */
-export function isPalindrome(x: number): boolean {
+const isPalindrome = (x: number): boolean => {
   return Number(x.toString().split('').reverse().join('')) === x
 }
 
-/** Not sure
- */
-export function isPalindromeWithoutStringConversion(x: number): boolean {
-  return true
-}
+export { isPalindrome }
