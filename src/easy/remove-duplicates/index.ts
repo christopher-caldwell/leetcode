48d5b98fb933678dddb51c138af654df3dcf9f22
christopher-caldwell <well.done.514@gmail.com>
2021-11-24 22:57:38 -0700

    fix: Removing extra loop

--- src/easy/remove-duplicates/index.ts
+++ src/easy/remove-duplicates/index.ts
@@ -1,17 +1,15 @@
 const removeDuplicates = (nums: number[]): number => {
+  let position = 0
   const uniquePositionMap: Record<number, number> = {}
   for (let i = 0; i < nums.length; i++) {
     if (uniquePositionMap[nums[i]] === undefined) {
       uniquePositionMap[nums[i]] = i
+      nums[position] = nums[i]
+      position++
     }
   }
+  nums.splice(position, nums.length - 1)
 
-  const entries = Object.entries(uniquePositionMap)
-  for (let i = 0; i < entries.length; i++) {
-    const [uniqueNum] = entries[i]
-    nums[i] = Number(uniqueNum)
-  }
-  nums.splice(entries.length, nums.length - 1)
   return nums.length
 }
 
